4b9379804ab32a8743758e3ab099e39ebc89dfb4
Tuncay Win Home <tunc270@gmail.com>
2020-05-11 02:30:42 +0300

    up

--- php-dizi-fonksiyonlar-2/php-dizi-fonksiyonlar-2.php
+++ php-dizi-fonksiyonlar-2/php-dizi-fonksiyonlar-2.php
@@ -20,12 +20,30 @@ function filtrele($val){
     return $val . ' -';
 }
 
+/*
+Array
+(
+    [0] => 1 -
+    [1] => 2 -
+    [2] => 3 -
+    [3] => 4 -
+    [4] => 5 -
+)
+*/
+
+
+/*
+
+*/
+
+
 $arr = [1,2,3,4,5];
 $arr2 = array_map('filtrele', $arr);
 $arr2 = array_map(function($val){
     return $val . ' -';
 }, $arr);
-//print_r($arr2);
+
+print_r($arr2);
 
 $arr = [1,2,3,4,5];
 $arr2 = array_filter($arr, function($item){
@@ -36,13 +54,15 @@ $arr2 = array_map(function($val){
         return $val;
     }
 }, $arr);
-//print_r($arr2);
+
+print_r($arr2);
 
 $arr1 = [1,2,3];
 $arr2 = [4,5,6];
 
 $arr = array_merge($arr1, $arr2);
-//print_r($arr);
+
+print_r($arr);
 
 $arr = [
     'ad' => 'tayfun',
@@ -50,17 +70,21 @@ $arr = [
     'yas' => 24,
     'site' => 'erbilen.net'
 ];
+
 $random = array_rand($arr, 2);
 $values = array_map(function($key) use($arr){
     return $arr[$key];
 }, $random);
 
-//print_r($values);
+print_r($values);
 
 $arr = [1,2,3,4,5];
-//print_r($arr);
+
+print_r($arr);
+
 $arr = array_reverse($arr);
-//print_r($arr);
+
+print_r($arr);
 
 $arr = [
     'ad' => 'tayfun',
@@ -88,46 +112,137 @@ function _array_search($cur_val, $arr)
 }
 
 $test = _array_search('d', $arr);
-//echo $test;
+
+echo $test . "\n";
 
 $arr = [1,2,3,4];
 
-/*
 if (in_array('6', $arr))
 {
-    echo '3 değeri var';
+    echo '6 değeri var' . "\n";
 } else {
-    echo 'yok';
+    echo 'yok' . "\n";
 }
-*/
+
 
 $arr = [1,2,3,4,5];
-//$ilk_eleman = array_shift($arr);
+$ilk_eleman = array_shift($arr);
+echo $ilk_eleman."\n";
 $son_eleman = array_pop($arr);
-//print_r($arr);
-//echo $son_eleman;
-//echo $ilk_eleman;
+echo $son_eleman . "\n";
+print_r($arr);
+
 
 $arr = [1,2,3,4,5];
 
 // ilk 2 eleman hariç hepsi
 $arr2 = array_slice($arr, 2);
-//print_r($arr2);
+print_r($arr2);
 
 $arr3 = array_slice($arr, 2, 2);
-//print_r($arr3);
+print_r($arr3);
 
 $arr4 = array_slice($arr, -2);
-//print_r($arr4);
+print_r($arr4);
 
 $arr = [1,2,3,4,5];
 $toplam = array_sum($arr);
-//echo $toplam;
+echo $toplam;
 
 $carpim = array_product($arr);
-//echo $carpim;
+echo $carpim;
 
 $arr = ['tayfun','erbilen','tayfun','erbilen','udemy'];
 print_r($arr);
 $arr2 = array_unique($arr);
 print_r($arr2);
+/*
+
+Array
+(
+    [0] =>
+    [1] =>
+    [2] => 3
+    [3] => 4
+    [4] =>
+)
+Array
+(
+    [0] => 1
+    [1] => 2
+    [2] => 3
+    [3] => 4
+    [4] => 5
+    [5] => 6
+)
+Array
+(
+    [0] => 24
+    [1] => erbilen.net
+)
+Array
+(
+    [0] => 1
+    [1] => 2
+    [2] => 3
+    [3] => 4
+    [4] => 5
+)
+Array
+(
+    [0] => 5
+    [1] => 4
+    [2] => 3
+    [3] => 2
+    [4] => 1
+)
+
+1
+yok
+1
+5
+
+Array
+(
+    [0] => 2
+    [1] => 3
+    [2] => 4
+)
+Array
+(
+    [0] => 3
+    [1] => 4
+    [2] => 5
+)
+
+Array
+(
+    [0] => 3
+    [1] => 4
+)
+
+Array
+(
+    [0] => 4
+    [1] => 5
+)
+
+15120
+
+Array
+(
+    [0] => tayfun
+    [1] => erbilen
+    [2] => tayfun
+    [3] => erbilen
+    [4] => udemy
+)
+Array
+(
+    [0] => tayfun
+    [1] => erbilen
+    [4] => udemy
+)
+
+*/
+
