3a663ce3e4805934f316faa2108329e9c8568c67
Tuncay <tunc270@gmail.com>
2023-10-20 15:00:38 +0300

    up

--- fonksiyon-3-getArg/php-fonksiyon-2.php
+++ fonksiyon-3-getArg/php-fonksiyon-2.php
@@ -1,18 +1,31 @@
 <?php
 
-    /*
-        func_num_args()
-        func_get_args()
-        func_get_arg()
-    */
+/*
+    func_num_args()
+    func_get_args()
+    func_get_arg()
+*/
 
-    function test()
-    {
-        echo func_num_args() . '<br>';
-        print_r(func_get_args()) . '<br>';
-        echo func_get_arg(2);
-    }
+function test()
+{
+  echo func_num_args() . '<br>';
+  print_r(func_get_args()) . '<br>';
+  echo func_get_arg(2);
+}
 
-    test('tayfun', 'udemy', 'prototurk', '93academy');
+test('tayfun', 'udemy', 'prototurk', '93academy');
+
+// **Output**
+/* 
+4
+Array
+(
+    [0] => tayfun
+    [1] => udemy
+    [2] => prototurk
+    [3] => 93academy
+)
+prototurk
+*/
 
 ?>
