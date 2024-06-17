c67db511c6474632a410512f8742dfc59dd10ecd
Tuncay_hp <tunc270@gmail.com>
2024-06-17 13:29:27 +0300

    up

--- pdo-demo2/index.php
+++ pdo-demo2/index.php
@@ -7,6 +7,7 @@ require_once ("./pdo-config.php");
 //require_once ("./pdo-create-table.php");
 //require_once ("./pdo-insert.php");
 //require_once ("./pdo-insert-transaction.php");
-require_once ("./pdo-prepared-stm.php");
+//require_once ("./pdo-prepared-stm.php");
+require_once ("./pdo-select.php");
 
 
---
+++ pdo-demo2/pdo-select.php
@@ -0,0 +1,27 @@
+<?php
+
+try {
+
+  $stmt = $pdo->prepare("SELECT id, firstname, lastname FROM MyGuests");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+
+  foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+    foreach ($row as $field => $value) {
+      echo $field . ' => ' . $value . '<br/>';
+    }
+  }
+
+  // foreach (new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k => $v) {
+  //   echo $v;
+  // }
+
+} catch (PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+
+$pdo = null;
+
+//echo "</table>";
