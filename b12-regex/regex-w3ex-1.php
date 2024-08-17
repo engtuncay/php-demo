e09c4733dbb3189134279e2d326a3a445c5053f1
Tuncay_hp <tunc270@gmail.com>
2024-08-17 10:54:36 +0300

    up

--- b12-regex/regex-w3ex-1.php
+++ b12-regex/regex-w3ex-1.php
@@ -12,6 +12,8 @@ if (preg_match($pattern, $str)) {
 
 echo "\n";
 
+//////////////////////////////////////////////
+////////////////////////////////////////////// 
 
 
 // preg_match_all example : pattern'in sayısını verir
@@ -21,12 +23,47 @@ $pattern = "/ain/i";
 echo preg_match_all($pattern, $str);
 
 echo "\n";
+echo "\n";
+
+//////////////////////////////////////////////  
+
+// preg_match ile eşleşen parçaları bulma
+
+preg_match_all($pattern, $str, $matches, PREG_SET_ORDER);
+
+foreach ($matches as $m) {
+  echo $m[0];
+  echo "\n";
+}
+
+echo "\n";
+echo "Gruplu bulma\n\n";
+
+$str2 = "The rain in SPAIN falls mainly on the plains.";
+$pattern2 = "/f(all).*(ain)/i";
+
+preg_match_all($pattern2, $str2, $matches2, PREG_SET_ORDER);
+
+foreach ($matches2 as $m) {
+  echo "m0:" . $m[0];
+  echo "\n";
+  if (!empty($m[1])) {
+    echo "m1:" . $m[1];
+    echo "\n";
+  }
+}
+
+echo "\n";
+
+//////////////////////////////////////////////  
 
 $str = "Visit Microsoft!";
 $pattern = "/microsoft/i";
 
 echo preg_replace($pattern, "W3Schools", $str);
 
+
+//////////////////////////////////////////////
 
 
 
