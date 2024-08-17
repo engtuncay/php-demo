fc66849c74b2ed5266114b2a66c9298bb9c7fd8f
Tuncay_hp <tunc270@gmail.com>
2024-08-17 09:58:56 +0300

    up

--- b12-regex/regex-w3ex-1.php
+++ b12-regex/regex-w3ex-1.php
@@ -1,5 +1,5 @@
 <?php
-
+// preg_match() example : pattern olup olmadığını döner - varsa 1, yoksa 0 döner
 $str = "Visit W3Schools";
 $pattern = "/w3schools/i"; // i -> case-insensitive
 echo preg_match($pattern, $str);
@@ -12,6 +12,10 @@ if (preg_match($pattern, $str)) {
 
 echo "\n";
 
+
+
+// preg_match_all example : pattern'in sayısını verir
+
 $str = "The rain in SPAIN falls mainly on the plains.";
 $pattern = "/ain/i";
 echo preg_match_all($pattern, $str);
@@ -22,5 +26,7 @@ $str = "Visit Microsoft!";
 $pattern = "/microsoft/i";
 
 echo preg_replace($pattern, "W3Schools", $str);
+
+
 
 
