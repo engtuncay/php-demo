f8a7e73b21fc7349bafec34f8c3e6c9a88df35ef
Tuncay <tunc270@gmail.com>
2024-09-10 15:34:13 +0300

    up

--- tordemo1/pdo/pdo-db-vars2.php
+++ tordemo1/pdo/pdo-db-vars2.php
@@ -1,15 +1,17 @@
 <?php
 //echo __DIR__;
-require_once("../torLib/pdo/TorConnConfig.php");
+use Engtuncay\Phputils\pdo\FiConnConfig;
+
+//require_once("../torLib/pdo/TorConnConfig.php");
 
 class WebSiteConfig {
-    public static TorConnConfig $torDbConfig;
+    public static FiConnConfig $torDbConfig;
 }
 
-WebSiteConfig::$torDbConfig = new TorConnConfig();
+WebSiteConfig::$torDbConfig = new FiConnConfig();
 WebSiteConfig::$torDbConfig->setServerName("localhost");
 WebSiteConfig::$torDbConfig->setUserName("root");
 WebSiteConfig::$torDbConfig->setUserPass(""); //tor91453
-WebSiteConfig::$torDbConfig->setDbName("myDB");
+WebSiteConfig::$torDbConfig->setDbName("test");
 
 ?>
